6e889265040fe5d263890db890383002ecde1e72
chattedlx <76582762+chattedlx@users.noreply.github.com>
2025-03-17 19:50:10 +0800

    Update ConvTR.py

--- ConvTR_LiaoXin/ConvTR.py
+++ ConvTR_LiaoXin/ConvTR.py
@@ -22,8 +22,6 @@ class ConvTRNet(torch.nn.Module):
                                                       embedding_dim=self.latent_dim * self.latent_dim)
 
         self.net = torch.nn.Sequential(
-
-            # 卷积
             torch.nn.Conv2d(1, 20, (10, 10), stride=10),
             torch.nn.BatchNorm2d(20, eps=1e-08),
             torch.nn.Dropout2d(0.1),
@@ -39,8 +37,7 @@ class ConvTRNet(torch.nn.Module):
             torch.nn.Linear(20, 20),
             torch.nn.ReLU(),
             torch.nn.Linear(20, 1),
-            # torch.nn.ReLU()
-
+            torch.nn.Sigmoid()
         )
         self.init_weight()
 
@@ -55,7 +52,6 @@ class ConvTRNet(torch.nn.Module):
         rating = torch.cat((user_slice, service_slice, timeslice_slice), dim=2)
         rating = rating.unsqueeze(1)
         rating = self.net(rating)
-
         return rating
 
     def init_weight(self):
@@ -64,39 +60,6 @@ class ConvTRNet(torch.nn.Module):
         torch.nn.init.uniform_(self.embedding_user.weight, 0.0, 0.004)
         torch.nn.init.uniform_(self.embedding_service.weight, 0.0, 0.004)
         torch.nn.init.uniform_(self.embedding_timeslice.weight, 0.0, 0.004)
-
-    def load_model(self, model_dir: str):
-        """Loading weights from trained model"""
-        config = self.config
-
-        conv_model = ConvTRNet(config)
-        if config["use_cuda"] is True:
-            conv_model.to(torch.device("cuda"))
-            device = "cuda"
-        else:
-            device = "cpu"
-
-        state_dict = torch.load(
-            model_dir,
-            map_location=torch.device(device),
-        )
-        conv_model.load_state_dict(state_dict)
-
-        self.embedding_user.weight.data = conv_model.embedding_user.weight.data
-        self.embedding_service.weight.data = conv_model.embedding_service.weight.data
-        self.embedding_timeslice.weight.data = conv_model.embedding_timeslice.weight.data
-
-        for idx in range(len(self.net) - 1):
-            if isinstance(self.net[idx], torch.nn.Conv2d):
-                self.net[idx].weight.data = conv_model.net[idx].weight.data
-                self.net[idx].bias.data = conv_model.net[idx].bias.data
-            if isinstance(self.net[idx], torch.nn.Linear):
-                self.net[idx].weight.data = conv_model.net[idx].weight.data
-                self.net[idx].bias.data = conv_model.net[idx].bias.data
-            if isinstance(self.net[idx], torch.nn.BatchNorm2d):
-                self.net[idx].weight.data = conv_model.net[idx].weight.data
-                self.net[idx].bias.data = conv_model.net[idx].bias.data
-
 
 class ConvTR(TFModel):
     """Engine for training & evaluating MLP model"""
